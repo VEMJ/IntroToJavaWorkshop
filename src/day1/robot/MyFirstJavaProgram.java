931687b9400d6c0f5a6b0a9e306218789dbd0af7
VEMJ <league@Mac-mini-3.local>
2016-08-15 15:01:40 -0700

    day one of coding 08/15/16

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
+Robot Veronica=new Robot();
+Veronica.move(124);	
+	Veronica.turn(90);	
+		Veronica.move(60);
+	Veronica.setPenColor(Color.MAGENTA);
+	Veronica.penDown();
+Veronica.move(90);	
+for (int i = 0; i < args.length; i++) {
+	for (int j = 0; j < args.length; j++) {
 		
 	}
-}
+}	
+	
+}}
